3b9f75465b4eefea6e533e5062299a4564620da7
coffee-qm <coffee.qm@gmail.com>
2017-10-24 23:45:43 +0800

    spring-jdbc-demo
    spring-jdbc-demo

--- src/main/java/com/coffee/common/db/demo/mo/DemoMo.java
+++ src/main/java/com/coffee/common/db/demo/mo/DemoMo.java
@@ -1,11 +1,23 @@
 package com.coffee.common.db.demo.mo;
 
+import java.util.Map;
+
 /**
  * @author QM
  */
 public class DemoMo {
 	private long id;
 	private String name;
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("\"id\":\"").append(this.id).append("\"");
+		sb.append(",\"name\":\"").append(this.name).append("\"");
+		sb.append("}");
+		return sb.toString();
+	}
 
 	public long getId() {
 		return id;
@@ -21,5 +33,12 @@ public class DemoMo {
 
 	public void setName(final String name) {
 		this.name = name;
+	}
+
+	public static DemoMo buildRecord(final Map<String, Object> record) {
+		final DemoMo mo = new DemoMo();
+		mo.setId(Long.valueOf(String.valueOf(record.get("id"))));
+		mo.setName(String.valueOf(record.get("name")));
+		return mo;
 	}
 }
