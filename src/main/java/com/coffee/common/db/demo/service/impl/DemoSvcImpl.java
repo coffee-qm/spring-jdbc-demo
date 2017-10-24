587e6ae0488d58ec981c68f332ec84c1c6abd6c4
coffee-qm <coffee.qm@gmail.com>
2017-10-24 23:46:31 +0800

    spring-jdbc-demo
    spring-jdbc-demo

--- src/main/java/com/coffee/common/db/demo/service/impl/DemoSvcImpl.java
+++ src/main/java/com/coffee/common/db/demo/service/impl/DemoSvcImpl.java
@@ -2,6 +2,11 @@ package com.coffee.common.db.demo.service.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
 import com.coffee.common.common.exp.SvcException;
 import com.coffee.common.db.demo.dao.DemoDao;
 import com.coffee.common.db.demo.mo.DemoMo;
@@ -10,15 +15,16 @@ import com.coffee.common.db.demo.service.DemoSvc;
 /**
  * @author QM
  */
+@Service("demoSvc")
+@Scope("prototype")
 public class DemoSvcImpl implements DemoSvc {
+	@Resource(name = "demoDao")
 	private DemoDao dao;
-
-	public void setDao(final DemoDao dao) {
-		this.dao = dao;
-	}
 
 	@Override
 	public List<DemoMo> query() throws SvcException {
-		return dao.query();
+		final List<DemoMo> lst = dao.query();
+		System.out.println(lst);
+		return lst;
 	}
 }
